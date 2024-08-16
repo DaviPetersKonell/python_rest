1244f43a45e5f986b4cb914b273164ae33caaaea
davipeterskonell <davipeterskonell@gmail.com>
2024-08-15 21:41:33 -0300

    Projeto Rest

---
+++ app.py
@@ -0,0 +1,153 @@
+from flask import Flask, jsonify, request
+
+from cadastro_autor import (alterar_autor_bd, consultar_autor_por_id_bd,
+                            deletar_autor_bd, inserir_autor_bd, listar_autores)
+from cadastro_livro import (alterar, consultar, consultar_por_id, deletar,
+                            inserir)
+from cadastro_usuario import (alterar_usuario_bd, consultar_usuario_por_id_bd,
+                              deletar_usuario_bd, inserir_usuario_bd,
+                              listar_usuarios, verificar_login)
+
+from cadastro_editora import inserir_editora_bd
+
+from conexao import conecta_db
+
+app = Flask(__name__)
+
+@app.route("/livros/<int:id>", methods=["GET"])
+def get_livro(id):
+   conexao = conecta_db()
+   livros = consultar_por_id(conexao,id)
+   return jsonify(livros)
+
+@app.route("/livros", methods=["POST"])
+def inserir_livro():
+    conexao = conecta_db()
+    data = request.get_json()
+    nome=data["nome"]
+    id_editora=data["id_editora"]
+    id_autor=data["id_autor"]
+    inserir(conexao,nome,id_editora,id_autor)
+    return jsonify(data)
+
+@app.route("/livros/<int:id>", methods=["PUT"])
+def alterar_livro(id):
+    conexao = conecta_db()
+    data = request.get_json()
+    nome=data["nome"]
+    alterar(conexao,int(id),nome)
+    return jsonify(data)
+
+
+@app.route("/livros/<int:id>", methods=["DELETE"])
+def excluir_livro(id):
+    conexao = conecta_db()
+    deletar(conexao,id)
+    return jsonify({"message": "Livro deletado com sucesso" })
+
+
+@app.route("/livros", methods=["GET"])
+def listar_todos():
+    conexao = conecta_db()
+    livros = consultar(conexao)
+    return jsonify(livros)
+
+@app.route("/autores", methods=["GET"])
+def listar_todos_autores():
+    conexao = conecta_db()
+    autores = listar_autores(conexao)
+    return jsonify(autores)
+
+
+@app.route("/autores", methods=["POST"])
+def inserir_autor():
+    conexao = conecta_db()
+    data = request.get_json()
+    nome = data["nome"]
+    inserir_autor_bd(conexao,nome)
+    return jsonify(data)
+
+@app.route("/autores/<int:id>", methods=["PUT"])
+def update_autor(id):
+    conexao = conecta_db()
+    data = request.get_json()
+    nome = data["nome"]
+    alterar_autor_bd(conexao,int(id),nome)
+    return jsonify(data)
+
+
+@app.route("/autores/<int:id>", methods=["DELETE"])
+def excluir_autor(id):
+    conexao = conecta_db()
+    deletar_autor_bd(conexao,id)
+    return jsonify({"message": "Autor deletado com sucesso" })
+
+
+@app.route("/autores/<int:id>", methods=["GET"])
+def consultar_autor_por_id(id):
+    conexao = conecta_db()
+    autor = consultar_autor_por_id_bd(conexao,id)
+    return jsonify(autor)
+
+
+@app.route("/usuarios", methods=["GET"])
+def listar_todos_usuarios():
+    conexao = conecta_db()
+    usuarios = listar_usuarios(conexao)
+    return jsonify(usuarios)
+
+
+@app.route("/usuarios", methods=["POST"])
+def inserir_usuario():
+    conexao = conecta_db()
+    data = request.get_json()
+    login = data["login"]
+    senha = data["senha"]
+    inserir_usuario_bd(conexao,login,senha)
+    return jsonify(data)
+
+@app.route("/usuarios/<int:id>", methods=["PUT"])
+def update_usuario(id):
+    conexao = conecta_db()
+    data = request.get_json()
+    login = data["login"]
+    senha = data["senha"]
+    alterar_usuario_bd(conexao,int(id),login,senha)
+    return jsonify(data)
+
+
+@app.route("/usuarios/<int:id>", methods=["DELETE"])
+def excluir_usuario(id):
+    conexao = conecta_db()
+    deletar_usuario_bd(conexao,id)
+    return jsonify({"message": "Usuário deletado com sucesso" })
+
+
+@app.route("/usuarios/<int:id>", methods=["GET"])
+def consultar_usuario_por_id(id):
+    conexao = conecta_db()
+    autor = consultar_usuario_por_id_bd(conexao,id)
+    return jsonify(autor)
+
+
+@app.route("/autenticar", methods=["POST"])
+def autenticar():
+    conexao = conecta_db()
+    data = request.get_json()
+    login = data["login"]
+    senha = data["senha"]
+    resultado = verificar_login(conexao,login,senha)
+    return jsonify(resultado)
+
+@app.route("/editoras", methods=["POST"])
+def inserir_editora():
+    conexao = conecta_db()
+    data = request.get_json()
+    nome = data["nome"]
+    inserir_editora_bd(conexao,nome)
+    return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ cadastro_livro.py
@@ -0,0 +1,57 @@
+from conexao import conecta_db
+
+# DML = Linguagem de manipulação de Dados
+#CRUD => Create, Read, Update , Delete
+#        INSERT, SELECT , UPDATE, DELETE 
+
+
+# DDL = Linguagem de Criação de tabelas
+#  Create table 
+#  DROP table
+#  Alter table 
+
+def consultar(conexao):
+    livros = []
+    cursor = conexao.cursor()
+    cursor.execute("select id,nome from livro")
+    registros = cursor.fetchall()
+    print("|-----------------------------------|")
+    for registro in registros:
+        item = {
+            "id": registro[0],
+            "nome": registro[1]
+        }
+        livros.append(item)
+    return livros
+
+
+def consultar_por_id(conexao, id):
+    cursor = conexao.cursor()
+    cursor.execute("select id,nome from livro where id = %s",[id])
+    registro = cursor.fetchone()
+    item = {
+        "id": registro[0],
+        "nome": registro[1]
+    }
+    return item
+
+def inserir(conexao, nome, id_editora,id_autor):
+    cursor = conexao.cursor()
+    sql_insert = "insert into livro (nome,id_editora,id_autor) values (%s, %s, %s)"
+    dados = (nome,id_editora,id_autor)
+    cursor.execute(sql_insert,dados)
+    conexao.commit()
+        
+def alterar(conexao, id, nome):
+    cursor = conexao.cursor()
+    sql_update = "update livro set nome = %s where id = %s"
+    dados   = (nome, id)
+    cursor.execute(sql_update,dados)
+    conexao.commit()    
+
+
+def deletar(conexao, id):
+    cursor = conexao.cursor()
+    sql_delete = "delete from  livro where id =  %s"
+    cursor.execute(sql_delete,[id])
+    conexao.commit()
